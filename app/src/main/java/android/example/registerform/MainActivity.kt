c47a48c01f7c40ff4f12370b3255ebe80ff3793e
Maalek.Kozoori <maalek.kozoori@gmail.com>
2022-01-20 11:10:04 +0330

    Refactor with viewBinding

--- app/src/main/java/android/example/registerform/MainActivity.kt
+++ app/src/main/java/android/example/registerform/MainActivity.kt
@@ -2,6 +2,7 @@ package android.example.registerform
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.example.registerform.databinding.ActivityMainBinding
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,56 +12,25 @@ import androidx.constraintlayout.widget.Group
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var txtFullname: EditText
-    lateinit var txtUserName: EditText
-    lateinit var txtEmail: EditText
-    lateinit var txtPass: EditText
-    lateinit var txtRetypePass: EditText
-    lateinit var btnRegister: Button
-    lateinit var btnShowInfo: Button
-    lateinit var rbFemale: RadioButton
-    lateinit var rbMale: RadioButton
-    lateinit var btnHideInfo: Button
-    lateinit var tvFullName: TextView
-    lateinit var tvUsername: TextView
-    lateinit var tvEmail: TextView
-    lateinit var tvPass: TextView
-    lateinit var tvGender: TextView
-    lateinit var infoGroup: Group
-    lateinit var genderGroup: Group
 
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        txtFullname = findViewById(R.id.txtFullName)
-        txtUserName = findViewById(R.id.txtUsername)
-        txtEmail = findViewById(R.id.txtEmail)
-        txtPass = findViewById(R.id.txtPass)
-        txtRetypePass = findViewById(R.id.txtRetypePass)
-        btnRegister = findViewById(R.id.btnRegister)
-        btnShowInfo = findViewById(R.id.btnShowInfo)
-        rbFemale = findViewById(R.id.rbFemale)
-        rbMale = findViewById(R.id.rbMale)
-        btnHideInfo = findViewById(R.id.btnHideInfo)
-        tvFullName = findViewById(R.id.tvFullName)
-        tvUsername = findViewById(R.id.tvUserName)
-        tvEmail = findViewById(R.id.tvEmail)
-        tvPass = findViewById(R.id.tvPassword)
-        tvGender = findViewById(R.id.tvGender)
-        infoGroup = findViewById(R.id.infoGroup)
-        genderGroup = findViewById(R.id.genderGroup)
+
         val sharedPreferences : SharedPreferences = this.getSharedPreferences("RegisterInfo",Context.MODE_PRIVATE)
 
-
+    with(binding){
         btnShowInfo.setOnClickListener {
             infoGroup.visibility = View.VISIBLE
             btnShowInfo.isEnabled = false
             tvFullName.text = sharedPreferences.getString("Full_Name","")
-            tvUsername.text = sharedPreferences.getString("User_Name","")
+            tvUserName.text = sharedPreferences.getString("User_Name","")
             tvEmail.text = sharedPreferences.getString("Email","")
-            tvPass.text = sharedPreferences.getString("Pass","")
+            tvPassword.text = sharedPreferences.getString("Pass","")
             tvGender.text = sharedPreferences.getString("Gender","")
         }
 
@@ -70,17 +40,22 @@ class MainActivity : AppCompatActivity() {
 
         var edit = sharedPreferences.edit()
         btnRegister.setOnClickListener {
-            edit.putString("Full_Name", txtFullname.text.toString())
-            edit.putString("User_Name", txtUserName.text.toString())
+            edit.putString("Full_Name", txtFullName.text.toString())
+            edit.putString("User_Name", txtUsername.text.toString())
             edit.putString("Email", txtEmail.text.toString())
             edit.putString("Pass", txtPass.text.toString())
             edit.putString("Gender", getGender())
             edit.apply()
-            Toast.makeText(this,"Data Saved Successfuly",Toast.LENGTH_LONG).show()
+            Toast.makeText(this@MainActivity,"Data Saved Successfuly",Toast.LENGTH_LONG).show()
+
         }
 
         rbFemale.setOnClickListener { rbMale.isChecked = false }
         rbMale.setOnClickListener { rbFemale.isChecked = false }
+
+
+
+    }
 
     }
 
@@ -88,17 +63,20 @@ class MainActivity : AppCompatActivity() {
 
 
     fun hideInfo() {
-        btnShowInfo.isEnabled = true
-        infoGroup.visibility = View.GONE
-
-
-
+        with(binding){
+            btnShowInfo.isEnabled = true
+            infoGroup.visibility = View.GONE
+        }
     }
 
     fun getGender(): String {
-        var gender = ""
-        if (rbFemale.isChecked) gender = "Female"
-        if (rbMale.isChecked) gender = "Male"
-        return gender
+
+        with(binding){
+            var gender = ""
+            if (rbFemale.isChecked) gender = "Female"
+            if (rbMale.isChecked) gender = "Male"
+            return gender
+        }
+
     }
 }
